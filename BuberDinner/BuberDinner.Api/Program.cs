9cec5fe5fcf32d3619cf110dddb8d7163e8469af
Erico Lira <ericocamposlira@gmail.com>
2022-08-29 15:25:59 +0100

    error handling youtube 4

--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -1,17 +1,24 @@
+using BuberDinner.Api.Filters;
+using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-    builder.Services.AddControllers();
+    
     builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
+
+    // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+    builder.Services.AddControllers();
 
 }
 
 var app = builder.Build();
 {
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
 
     app.MapControllers();
